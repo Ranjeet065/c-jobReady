2a6abba8e74e702080dc33ca9a35e520df74ab61
Ranjeet065 <109332696+Ranjeet065@users.noreply.github.com>
2022-07-16 19:27:21 +0530

    Add files via upload

---
+++ assignment4.c
@@ -0,0 +1,146 @@
+#include<stdio.h>
+#include<conio.h>
+/*
+// Question 1
+//Write a program to print MySirG 5 times on the screen
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return(0);
+}
+*/
+
+/*
+// Question 2
+// Write a program to print the first 10 natural numbers.
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\t",i);
+        i++;
+    }
+    getch();
+    return(0);
+}
+*/
+
+/*
+// Question 3
+// Write a program to print the first 10 natural numbers in reverse order
+int main()
+{
+    int i=10;
+    while(i)
+    {
+        printf("%d\t",i);
+        i--;
+    }
+    return(0);
+}
+*/
+
+/*
+//Question 4
+//Write a program to print the first 10 odd natural numbers
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d  ",2*i-1);
+    }
+    return(0);
+}
+*/
+
+/*
+// Question 5
+//Write a program to print the first 10 odd natural numbers in reverse order.
+int main()
+{
+    int i;
+    for(i=10;i>=1;i--)
+    {
+        printf("%d  ",2*i-1);
+    }
+    return(0);
+}
+*/
+
+/*
+// Question 6
+//Write a program to print the first 10 even natural numbers
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\t",2*i);
+        i++;
+    }
+    return(0);
+}
+*/
+
+/*
+//Question 7
+//Write a program to print the first 10 even natural numbers in reverse order
+int main()
+{
+    int i=10;
+    while(i)
+    {
+        printf("%d  ",2*i);
+        i--;
+    }
+    return(0);
+}
+*/
+
+/*
+//Question 8
+//Write a program to print squares of the first 10 natural numbers
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d  ",i*i);
+    }
+    return(0);
+}
+*/
+
+/*
+//Question 9
+//Write a program to print cubes of the first 10 natural numbers
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d  ",i*i*i);
+        i++;
+    }
+    return(0);
+}
+*/
+//Question 10;
+//Write a program to print a table of 5.
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("5 x %d = %d\n",i,5*i);
+    }
+    return(0);
+}
+ 
